947ad7f77a4a5c28cc3a3fc6d8d8345cc620e1a6
Alvergel25 <nicolasmg4012@gmail.com>
2025-06-12 18:41:32 +0200

    Game Manager
    Game manager me falta lo demas

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static GameManager instance;
+
+    private int cambiosDireccion = 0;
+
+    public int CambiosDireccion => cambiosDireccion; // propiedad pública solo de lectura
+
     void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // permanece entre escenas
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void RegistrarCambioDireccion()
+    {
+        cambiosDireccion++;
     }
 }
