4ad95d00f2e1d05151db0f7b8e2367d56345fea1
Nicolas07n <nicolasmg4012@gmail.com>
2025-06-13 19:20:53 +0200

    Enemy
    Enemigo

--- Assets/Scripts/OOP/Enemy.cs
+++ Assets/Scripts/OOP/Enemy.cs
@@ -6,5 +6,22 @@ public enum EnemyType { AVERAGE_ENEMY, RANDOM_ENEMY, DUMB_ENEMY };
 
 public abstract class Enemy
 {
-    
+    protected float speed;
+    protected Rigidbody2D rb;
+    protected Sprite sprite;
+
+    public Enemy(float speed, Rigidbody2D rb, Sprite sprite)
+    {
+        this.speed = speed;
+        this.rb = rb;
+        this.sprite = sprite;
+    }
+
+    public Sprite GetSprite()
+    {
+        return sprite;
+    }
+
+    public abstract void Move();
+    public virtual void Hit() { }
 }
