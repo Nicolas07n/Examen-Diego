df38aae76a1b5d4fb6730e04e5e81fe6a50f3f89
Nicolas07n <nicolasmg4012@gmail.com>
2025-06-14 21:46:14 +0200

    Dumb enemy
    si

--- Assets/Scripts/OOP/DumbEnemy.cs
+++ Assets/Scripts/OOP/DumbEnemy.cs
@@ -2,7 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DumbEnemy
+public class DumbEnemy:Enemy 
 {
-    
+    private Vector2 dir;
+
+    public DumbEnemy()
+    {
+        // Constructor vacío.
+    }
+
+    public DumbEnemy(Rigidbody2D rb) : base(30f, rb, null)
+    {
+        // Dirección aleatoria (unidad normalizada)
+        float x = Random.Range(-1f, 1f);
+        float y = Random.Range(-1f, 1f);
+        dir = new Vector2(x, y).normalized;
+    }
+
+    public override void Move()
+    {
+        rb.velocity = dir * speed;
+    }
+
+    public override void Hit()
+    {
+        // Aquí puedes destruir al enemigo o poner una animación de muerte
+        Debug.Log("DumbEnemy ha muerto.");
+        GameObject.Destroy(rb.gameObject);
+    }
 }
