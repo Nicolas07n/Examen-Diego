bd742eaf8b77f2e3e24afb674abb37426e4de472
Nicolas07n <nicolasmg4012@gmail.com>
2025-06-14 22:01:28 +0200

    RandomEnemy
    si

--- Assets/Scripts/OOP/RandomEnemy.cs
+++ Assets/Scripts/OOP/RandomEnemy.cs
@@ -4,9 +4,47 @@ using UnityEngine;
 
 public abstract class RandomEnemy : Enemy
 {
-    
+    private Vector2 dir;
+    private float timeToSpin;
+    private float currentTime;
+    private bool confused;
+
+    public RandomEnemy()
+    {
+        // Constructor vacío.
+    }
+
+    public RandomEnemy(Rigidbody2D rb, Vector2 dir, float timeToSpin) : base(10f, rb, null)
+    {
+        this.dir = dir.normalized;
+        this.timeToSpin = timeToSpin;
+        this.currentTime = 0f;
+        this.confused = false;
+    }
+
     public override void Move()
     {
-        
+        if (confused)
+        {
+            currentTime += Time.deltaTime;
+            rb.transform.Rotate(0, 0, 360f * Time.deltaTime); 
+
+            if (currentTime >= timeToSpin)
+            {
+                confused = false;
+                currentTime = 0f;
+                dir = -dir; 
+            }
+        }
+        else
+        {
+            rb.velocity = dir * speed;
+        }
+    }
+
+    public override void Hit()
+    {
+        confused = true;
+        currentTime = 0f;
     }
 }
